f255eff415f1004f8a3ea0bf0cc6ccf479bbf399
callum <callum.charteris.2024@uni.strath.ac.uk>
2026-01-28 12:53:10 +0000

    Styled Title Added

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,39 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View, StyleSheet } from "react-native";
+
+/*Can create style sheets, which is like CSS in html*/
+/*Can have all styles in your sheet with identifierss*/
+const style = StyleSheet.create({
+  title: {
+    //then you write CSS like stuff in here
+    color: 'black',
+    marginTop: 16,
+    borderWidth: 3,
+    padding: 3,
+    backgroundColor: '#61dafb',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
 
 export default function Index() {
   return (
 
-    //a view is basically the UI, the screen the user sees
+
+    /*a view is basically the UI, the screen the user sees
+    the style is self contained in the view tag*/
     <View
       style={{
-        flex: 1,
+        backgroundColor: 'white',
         justifyContent: "center",
         alignItems: "center",
+
+
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      <Text style={style.title}>InTune Shawlands</Text>
+
     </View>
   );
 }

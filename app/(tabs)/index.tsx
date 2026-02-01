a6d1a88187df241e0844930da99e9cdac0e3d814
callum <callum.charteris.2024@uni.strath.ac.uk>
2026-02-01 21:44:07 +0000

    Index flexbox and basic content
    Arranged content using flex to space everything. Styled header text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View, StyleSheet } from "react-native";
 import { Image } from 'expo-image';
+import { useFonts } from 'expo-font';
 
 /*Can create style sheets, which is like CSS in html*/
 /*Can have all styles in your sheet with identifierss*/
@@ -14,11 +15,20 @@ const style = StyleSheet.create({
     justifyContent: "center",
   },
 
+  header: {
+    //CSS stuff for the header
+    flex: 0.5,
+    padding: 15,
+    fontSize: 20,
+    fontFamily: 'Impact'
+  },
+
   image: {
     //CSS like stuff for the image
-    flex: 1,
-    width: '100%'
+    flex: 9,
+    width: '100%',
   },
+
 });
 
 export default function Index() {
@@ -28,7 +38,7 @@ export default function Index() {
     /*a view is basically the UI, the screen the user sees
     the style is self contained in the view tag*/
     <View style={style.main}>
-      <Text>Welcome to InTune Shawlands!</Text>
+      <Text style={style.header}>Welcome to InTune Shawlands!</Text>
 
       <Image
         style={style.image}
